4605c881dc7f73aa72070ebdeccd747528254adb
goranaviani <goranaviani@gmail.com>
2022-06-15 21:10:13 +0200

    react_props_children

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
   return (
     <div className="App">
       <Book name = {firstBook.name} author = {firstBook.author}>This is a child</Book>
+      <Book name = {firstBook.name} author = {firstBook.author}></Book>
+
     </div>
   );
 }
