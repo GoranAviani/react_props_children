72ae830642ba41f07ded4a846d6174a6b8a627ec
goranaviani <goranaviani@gmail.com>
2022-06-16 20:13:26 +0200

    react_props_children

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { useTheme,  } from "../App";
 
 const Description = () => {
-
-    return(<p>This is a description</p>)
-}
+const theme = useTheme()
+    console.log(theme)
+    return(
+        <div style={{color: theme.color}}>
+        <p>This is a description</p>
+        </div>
+    )}
 
 export default Description;
