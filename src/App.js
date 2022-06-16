ec49351a46902dbe9126145dbcb6c2e0484904ca
goranaviani <goranaviani@gmail.com>
2022-06-16 15:23:30 +0200

    react_props_children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Book from "./components/Book";
-
+import Description from "./components/Description"
 
 function App() {
     const firstBook = {
@@ -12,7 +12,6 @@ function App() {
     <div className="App">
       <Book name = {firstBook.name} author = {firstBook.author}>This is a child</Book>
       <Book name = {firstBook.name} author = {firstBook.author}></Book>
-
     </div>
   );
 }
